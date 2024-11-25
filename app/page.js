634e07846bd9106677926d3d5fc00ad1eeb132d4
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-11-25 11:49:45 +0700

    fisrt-commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Page() {
+  return (
+    <div>
+      <h1>The wilde Oasis. Welcome to paradise.</h1>
+      <Link href="/cabins">Expore luxury cabins</Link>
+    </div>
+  )
+}
